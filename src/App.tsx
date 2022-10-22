8a01a5a349ab7a318751d9a4324e5af034bea958
Batuhan Kacmaz <batuhan.kacmaz1105@gmail.com>
2022-10-23 00:13:17 +0300

    making folder structure and adding sass

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+
+import './App.scss'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <span className="header_text">Hi Im Header</span>
+      </div>
+      <div className="card">
+        <span className="card_name">Hi Im Card</span>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
